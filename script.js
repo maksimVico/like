e9ed54c55cf6c70928245da163fb8b5fc68e5a0a
maksimVico <146733573+maksimVico@users.noreply.github.com>
2023-10-07 14:57:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 let likeCount = parseInt(localStorage.getItem('likeCount')) || 0;
-let hasLiked = localStorage.getItem('hasLiked') === 'true' || false;
 
 const heart = document.getElementById('heart');
 const likes = document.getElementById('likes');
+const saveButton = document.getElementById('saveButton');
 
 function updateLikes() {
     likes.textContent = `${likeCount} ljudi je lajkovalo ovo srce.`;
 }
 
-if (hasLiked) {
-    heart.classList.add('liked');
-    heart.innerHTML = '&#10084;&#65039;';
-    updateLikes();
-}
-
 heart.addEventListener('click', () => {
-    if (!hasLiked && likeCount < 100) {
+    if (likeCount < 100) { // Provera da li je broj lajkova manji od 100
         likeCount++;
         localStorage.setItem('likeCount', likeCount.toString());
-        localStorage.setItem('hasLiked', 'true');
-        heart.classList.add('liked');
-        heart.innerHTML = '&#10084;&#65039;';
-        hasLiked = true;
-        updateLikes();
-    }
-    else if (hasLiked) {
-        likeCount--;
-        localStorage.setItem('likeCount', likeCount.toString());
-        localStorage.setItem('hasLiked', 'false');
-        heart.classList.remove('liked');
-        heart.innerHTML = '&#10084;';
-        hasLiked = false;
         updateLikes();
     }
 });
+
+saveButton.addEventListener('click', () => {
+    // Kopiranje broja lajkova u privremeno skladište
+    let savedLikes = parseInt(localStorage.getItem('savedLikes')) || 0;
+    savedLikes += likeCount;
+    localStorage.setItem('savedLikes', savedLikes.toString());
+    alert(`Broj lajkova (${likeCount}) je sačuvan. Sada imate ukupno ${savedLikes} lajkova.`);
+});
+
+updateLikes(); // Dodajte ovo kako biste ažurirali broj lajkova pri prvom učitavanju stranice
